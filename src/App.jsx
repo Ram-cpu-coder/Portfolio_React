1f5e0959081f4f6933d26119645a60d510aee4a3
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-05-15 10:48:00 +1000

    particles bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import StarField from "./components/StarField";
 import { useDispatch } from "react-redux";
 import { contributionFromDbAction } from "./feature/githubContributionAction";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 function App() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(contributionFromDbAction());
     localStorage.setItem("i18nextLng", "en");
   }, []);
   return (
-    <div className="position-relative vh-100 d-flex justify-content-center">
-      <div className="position-absolute background">
-        {/* this is the background */}
-        {/* <StarField /> */}
+    // <div className="position-relative vh-100 d-flex justify-content-center">
+    <div className="app-container">
+      <ParticlesBackground />
+      <div className="app-content">
+        <AppRoutes />
       </div>
-      <AppRoutes />
     </div>
+    // </div>
   );
 }
 
